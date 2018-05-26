54b49c74495591e893db2bf67e8848cee8d88ee5
NikitaLem <saintsuigintoufag@gmail.com>
2018-05-26 23:58:30 +0600

    favicons added/html-refactoring-fix

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,4 @@
-import greet from './greeter.js';
+import '../favicons/favicons.js';
 
-var html = require('../../blocks/index.pug')
+var html = require('../../blocks/index.pug');
 const css = require('../../blocks/index.styl');
-
-console.log("I'm the enty point");
-greet();
