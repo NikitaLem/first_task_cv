03bb936311ba817b8bfd996b4c3e4d018b075508
NikitaLem <saintsuigintoufag@gmail.com>
2018-10-03 19:57:04 +0600

    webpack.config.js отступы поправлены  #7

--- webpack.config.js
+++ webpack.config.js
@@ -2,58 +2,58 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'docs')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.styl$/, 
-                use: ['style-loader', 'css-loader?url=false', 'stylus-loader']
-            },
-            {
-                test: /\.pug$/,
-                use: ['raw-loader', 'pug-html-loader']
-            },
-            {
-                test: /node_modules\/JSONStream\/index\.js$/,
-                loaders: ['shebang', 'babel']
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,    //to support @font-face rule 
-                loader: "url-loader",
-                query:{
-                  limit:'10000',
-                  name:'[name].[ext]',
-                  outputPath:'fonts/'
-                }
-            },
-            {
-                test: /\.(jpe?g|png|gif)$/i,   //to support eg. background-image property 
-                loader:"file-loader",
-                query:{
-                  name:'[name].[ext]',
-                  outputPath:'images/'
-                }
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "docs"),
-        compress: true,
-        open: true,
-        stats: "errors-only"
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'For Lera',
-            minify: {
-                collapseWhitespace: true
-            },
-            hash: true,
-            template: 'src/index.pug',
-        })
-        ]
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'docs')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.styl$/, 
+        use: ['style-loader', 'css-loader?url=false', 'stylus-loader']
+      },
+      {
+        test: /\.pug$/,
+        use: ['raw-loader', 'pug-html-loader']
+      },
+      {
+        test: /node_modules\/JSONStream\/index\.js$/,
+        loaders: ['shebang', 'babel']
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,    //to support @font-face rule 
+        loader: "url-loader",
+        query:{
+          limit:'10000',
+          name:'[name].[ext]',
+          outputPath:'fonts/'
+        }
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,   //to support eg. background-image property 
+        loader:"file-loader",
+        query:{
+          name:'[name].[ext]',
+          outputPath:'images/'
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "docs"),
+    compress: true,
+    open: true,
+    stats: "errors-only"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'For Lera',
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      template: 'src/index.pug',
+    })
+  ]
 };
